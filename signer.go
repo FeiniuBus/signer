30604f97732ff6b5a701c5baa42814a311958e52
Jamesxql <Jamesxql@Gmail.com>
2017-11-14 15:11:11 +0800

    add hmac v1 signer

--- signer.go
+++ signer.go
@@ -14,7 +14,13 @@ type Request struct {
 	Host  string
 }
 
+// SigningResult is a signing result strcuture
+type SigningResult struct {
+	Signature string
+	Timestamp string
+}
+
 // A Signer is the interface for any component which will provide signature algorithm.
 type Signer interface {
-	Sign(r *Request, signTime time.Time, exp time.Duration)
+	Sign(r *Request, signTime time.Time, exp time.Duration) (*SigningResult, error)
 }
