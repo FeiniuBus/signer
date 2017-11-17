82efeca85c9df9071b00f5a95fbb3a76a6638a05
Andrew <mykge@outlook.com>
2017-11-17 15:41:08 +0800

    add Test_RSAServerParallel

--- rsa_server_test.go
+++ rsa_server_test.go
@@ -2,6 +2,8 @@ package signer
 
 import (
 	"encoding/base64"
+	"fmt"
+	"sync"
 	"testing"
 )
 
@@ -28,4 +30,39 @@ func Test_RSAServer(t *testing.T) {
 	signature, url, err := client.Sign([]byte("testing"))
 	t.Log(base64.StdEncoding.EncodeToString(signature))
 	t.Log(url)
+}
+
+func Test_RSAServerParallel(t *testing.T) {
+	root, err := Parsex509RSACert(getRootCA(), getRootKey())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	factory := NewRSAStoreFactory("test", "polaris/Certificates", root, GetDefaultSubject())
+	store, err := factory.Create(x509RSAStore_OneToMany)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+
+	f := func(clientID string) {
+		server := Newx509RSAServer(store)
+		client, err := server.CreateClient(clientID)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		signature, url, err := client.Sign([]byte("testing"))
+		t.Log(base64.StdEncoding.EncodeToString(signature))
+		t.Log(url)
+		wg.Done()
+	}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go f(fmt.Sprintf("test_client_%d", i))
+	}
+
+	wg.Wait()
 }
