0884dadb5f43068028177fe4832cb5959a9ee9a1
Andrew <mykge@outlook.com>
2017-11-21 10:05:48 +0800

    fix interface

--- rsa_client.go
+++ rsa_client.go
@@ -3,4 +3,5 @@ package signer
 type RSAClient interface {
 	Sign(input []byte) ([]byte, string, error)
 	ASN1Sign(typ string, payloads ...string) ([]byte, string, error)
+	StringsSign(separator string, payloads ...string) ([]byte, string, error)
 }
